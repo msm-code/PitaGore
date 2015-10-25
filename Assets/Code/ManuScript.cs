93c4e4b894cd7073d71bb0e511cdf96a799937b1
Regis128 <regis1286@gmail.com>
2015-10-25 10:20:09 +0100

    Manu part1

---
+++ Assets/Code/ManuScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ManuScript : MonoBehaviour {
+    public Button Adventure;
+    public Button Arena;
+    public Button Quit;
+
+    // Use this for initialization
+    void Start () {
+        Adventure = Adventure.GetComponent<Button>();
+        Arena = Arena.GetComponent<Button>();
+        Quit = Quit.GetComponent<Button>();
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PlayAdventure()
+    {
+
+    }
+
+    public void PlayArena()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
